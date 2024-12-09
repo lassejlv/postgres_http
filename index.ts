fac7ce6b16aeb353e76dd9ce303248ccbd549ff6
lassejlv <vestergaardlasse2@gmail.com>
2024-12-09 12:36:29 +0100

    changes

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { zValidator } from '@hono/zod-validator'
 import { Pool } from 'pg'
 import { z } from 'zod'
+import { createMiddleware } from 'hono/factory'
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -14,7 +15,26 @@ const schema = z.object({
   args: z.array(z.any()).optional(),
 })
 
-app.post('/query', zValidator('json', schema), async (c) => {
+
+const authMiddleware = createMiddleware(async (c, next) => {
+
+  try {
+    const auth = c.req.header("Authorization")
+    if (!auth) throw new Error('No authorization header')
+
+    const [type, token] = auth.split(' ')
+    if (type !== 'Bearer') throw new Error('Invalid authorization type')
+
+    const API_KEY = process.env.API_KEY
+    if (token !== API_KEY) throw new Error('Invalid API key')
+
+    return next()
+  } catch (error: any) {
+    return c.json({ error: error.message }, 401)
+  }
+})
+
+app.post('/query', authMiddleware, zValidator('json', schema), async (c) => {
   try {
     const { query, args } = c.req.valid('json')
 
